e02fd826a5894091aa85d5adb092fc8277a7e734
Stuart Kennedy <stuart.kennedy@qrious.co.nz>
2023-05-06 14:28:56 +1200

    Remove redundant crypto usage

--- src/github-app-auth/main.ts
+++ src/github-app-auth/main.ts
@@ -1,4 +1,3 @@
-import { createPrivateKey } from "node:crypto";
 import { inspect } from "node:util";
 import { getInput, setSecret, setOutput, setFailed, debug } from "@actions/core";
 import { createAppAuth } from "@octokit/auth-app";
@@ -7,9 +6,7 @@ try {
   const appId = getInput("app-id", { required: true });
   const privateKey = getInput("private-key", { required: true });
 
-  const privateKeyPkcs8 = createPrivateKey(privateKey).export({ type: "pkcs8", format: "pem" });
-
-  const auth = createAppAuth({ appId, privateKey: privateKeyPkcs8.toString("utf8") });
+  const auth = createAppAuth({ appId, privateKey });
 
   const appAuthentication = await auth({ type: "app" });
 
